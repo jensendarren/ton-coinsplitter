9765384f4fabb171d36c39518b066612d3db218d
Darren Jensen <darren.jensen@gmail.com>
2023-02-18 13:28:36 +0700

    finished deployer script

--- scripts/deployDistributor.ts
+++ scripts/deployDistributor.ts
@@ -1,13 +1,23 @@
-import { toNano } from 'ton-core';
+import { Address, toNano } from 'ton-core';
 import { Distributor } from '../wrappers/Distributor';
 import { compile, NetworkProvider } from '@ton-community/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const distributor = Distributor.createFromConfig({}, await compile('Distributor'));
+    let myAddress = Address.parse('EQBO_BKSeVRZzRhfuj3hqF-ez4g3T0af7xXCk0cve35ps83v')
+    
+    const distributor = Distributor.createFromConfig({
+        owner: myAddress,
+        processingPrice: toNano('0.01'),
+        seed: 0,
+        shares: [
+            { address: myAddress, factor: 1, base: 2, comment: 'first half' },
+            { address: myAddress, factor: 1, base: 2, comment: 'second half' }
+        ]
+    }, await compile('Distributor'));
 
     await provider.deploy(distributor, toNano('0.05'));
 
     const openedContract = provider.open(distributor);
 
-    // run methods on `openedContract`
+    console.log(`Distributer is deployer at: ${openedContract.address}`);
 }
